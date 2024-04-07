1ab8fe49e169ac253f0f2336fb87be2bd92ac33e
Agustina Castillo <agustinacastillo919@gmail.com>
2024-04-07 16:09:58 -0300

    agregur el total de productos, falta el de categorias

--- src/components/pages/TotalDeCategorias.jsx
+++ src/components/pages/TotalDeCategorias.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
 
 function TotalDeCategorias(){
+
+const [data, setData] = useState([]);
+
+useEffect(()=>{
+    fetch('http://localhost:3000/api/products')
+    .then((response)=> response.json())
+    .then((data)=> setData(data))
+    .catch((error)=>console.log(error))
+}, []);
+
     return(
         <div>
-        <h2>Total de Categorias aqui</h2>
+        <h2>Total de Categorias en Digital Clothes: </h2> 
         </div>
     )
+
+
 }
 export default TotalDeCategorias
 
--- src/components/pages/TotalDeProductos.jsx
+++ src/components/pages/TotalDeProductos.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
 
 
 function TotalDeProductos(){
+
+const [data, setData] = useState([]);
+
+useEffect(()=>{
+    fetch('http://localhost:3000/api/products')
+    .then((response)=> response.json())
+    .then((data)=> setData(data))
+    .catch((error)=>console.log(error))
+}, []);
+
     return(
         <div>
-        <h2>Total de Productos aqui</h2>
+        <h2>Total de Productos en Digital Clothes: {data.countProduct}</h2> 
         </div>
     )
 };
