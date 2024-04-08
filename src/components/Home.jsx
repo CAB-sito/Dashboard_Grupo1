06cbde8c23b436c43d706b3200a25d6031613a0d
Brian <carabajalbrianagustin2@gmail.com>
2024-04-07 21:45:18 -0300

    .

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ function Home(){
     return(
         <div className='Home'>
              
-       <h2> <Link to="/totalCategorias" >Categorias</Link> <br /></h2>
+       <h2> <Link to="/totalCategorias" >Total de Categorias</Link> <br /></h2>
        <h2> <Link to="/totalUsuarios">Total de usuarios</Link><br /></h2>
        <h2> <Link to="/totalProductos">Total de productos</Link><br /></h2>
        <h2> <Link to="/ultimoUsuario">Ultimo usuario creado</Link><br /></h2>
